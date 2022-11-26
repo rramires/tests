4b2aa0010b46c05fe1ad6c2ebcef8dea3c18164f
rramires <ricardo@flexbr.com>
2022-11-26 14:51:12 -0300

    Add Redis test code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Testando 123...')
+// Teste de cache no Redis
+(async () => {
+ 
+    const redis = require('redis');
+    const client = redis.createClient();
+    client.connect();
+ 
+    client.on("error", (error) => {
+        console.error(error);
+    });
+ 
+    const res = await client.set("key", "Valor123");
+    console.log(`Result do set: ${res}`);
+ 
+    const key = await client.get("key");
+    console.log(`Result do get: ${key}`);
+ 
+})();
